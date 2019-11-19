e87ee29b8ac6890e1b40b101f3e16f96196dcce5
Wofra <toni-mr-1996@hotmail.com>
2019-11-19 21:53:44 +0100

    Pausa
    Afegit menu de pausa i preparat per fer menu inici

---
+++ Assets/Scipts/Player/PlayerScenes.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScenes : MonoBehaviour
+{
+    [SerializeField] GameObject menuPausa;
+    [SerializeField] PlayerCam camara;
+    [SerializeField] GameObject CrossAir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuPausa.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!menuPausa.activeSelf) Pausar();
+            else Renaudar();
+
+        }
+    }
+
+    public void ResetejarEscena()
+    {
+        Renaudar();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Sortir()
+    {
+        Application.Quit();
+    }
+    
+    public void TornarMenu()
+    {
+        Renaudar();
+        SceneManager.LoadScene(0);
+    }
+
+    public void Pausar()
+    {
+        Time.timeScale = 0;
+        menuPausa.SetActive(true);
+        camara.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        CrossAir.SetActive(false);
+    }
+
+    public void Renaudar()
+    {
+        Time.timeScale = 1;
+        menuPausa.SetActive(false);
+        camara.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        CrossAir.SetActive(true);
+    } 
+
+    void SeguentEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
